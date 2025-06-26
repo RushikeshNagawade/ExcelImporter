51d7e2d72b470c3e9413c711f823f757aa1a5fcf
RushikeshNagawade <85493813+RushikeshNagawade@users.noreply.github.com>
2025-06-26 13:05:28 +0530

    excel Importer project starting point

---
+++ src/main/java/personal/rushikesh/excelImporter/ExcelImporterApplication.java
@@ -0,0 +1,13 @@
+package personal.rushikesh.excelImporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/rushikesh/excelImporter/ExcelImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.rushikesh.excelImporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
